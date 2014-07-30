0328c880355af6d1ad3126c412258e2474945bb9
tallendev <tallendev@gmail.com>
2014-07-30 03:15:22 -0400

    Testing dependencies...

---
+++ src/clojuresite/handler.clj
@@ -0,0 +1,25 @@
+(ns clojuresite.handler
+  (:require [compojure.core :refer [defroutes routes]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [clojuresite.routes.home :refer [home-routes]]))
+
+(defn init []
+  (println "clojuresite is starting"))
+
+(defn destroy []
+  (println "clojuresite is shutting down"))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (routes home-routes app-routes)
+      (handler/site)
+      (wrap-base-url)))
+
+
---
+++ src/clojuresite/views/layout.clj
@@ -0,0 +1,9 @@
+(ns clojuresite.views.layout
+  (:require [hiccup.page :refer [html5 include-css]]))
+
+(defn common [& body]
+  (html5
+    [:head
+     [:title "Welcome to clojuresite"]
+     (include-css "/css/screen.css")]
+    [:body body]))
