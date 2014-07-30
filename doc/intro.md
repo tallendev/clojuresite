da62f7ba00c52d4c68eb09ed0d858b614b185a6c
Tyler Allen <xtallenz@gmail.com>
2014-07-29 21:50:46 -0400

    First commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojuresite
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojuresite/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojuresite.core-test
+  (:require [clojure.test :refer :all]
+            [clojuresite.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
